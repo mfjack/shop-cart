8b95fc4561f863afc2ceb17876f756296ce3306f
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-17 20:52:40 -0300

    style: style cart icon

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -7,8 +7,9 @@ const Header = () => {
          <Link href="/">
             <IconBrandAmazon size={40} stroke={2} className="text-white" />
          </Link>
-         <Link href="/cart">
+         <Link href="/cart" className="relative">
             <IconShoppingCart size={35} stroke={2} className="text-white" />
+            <div className="absolute w-6 h-6 bg-red-500 rounded-full text-center text-white top-0 right-0 flex items-center text-xs justify-center">10</div>
          </Link>
       </header>
    );
