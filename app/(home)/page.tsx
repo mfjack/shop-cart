2d7d5fadc5d0af6de83ca82be376d178ca3a49bd
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-18 01:19:15 -0300

    feat: add product item component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import ProductItem from "../_components/product-item";
+import produtos from "@/data/constants/produtos";
+
 const HomePage = () => {
-  return (  );
-}
- 
+   return (
+      <div className="container mx-auto h-screen">
+         <div className="flex flex-wrap py-5 gap-5 justify-center">
+            {produtos.map((produto) => (
+               <ProductItem key={produto.id} produto={produto} />
+            ))}
+         </div>
+      </div>
+   );
+};
+
 export default HomePage;
---
+++ app/_components/product-item.tsx
@@ -0,0 +1,23 @@
+import Produto from "@/data/model/produto";
+import Image from "next/image";
+
+interface ProductItemProps {
+   produto: Produto;
+}
+
+const ProductItem = ({ produto }: ProductItemProps) => {
+   return (
+      <div className="border-2 p-2 rounded-lg bg-gray-100">
+         <Image src={produto.imagem} alt={produto.nome} width={200} height={200} className="object-cover" />
+         <div>
+            <div className="py-2 flex justify-between items-center">
+               <h2 className="font-semibold">{produto.nome}</h2>
+               <p className="text-sm">R$ {produto.preco}</p>
+            </div>
+            <button className="bg-blue-600 hover:bg-blue-500 rounded-md shadow-sm text-white px-2 py-1">Adicionar</button>
+         </div>
+      </div>
+   );
+};
+
+export default ProductItem;
