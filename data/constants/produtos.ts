bfb49a1dfed0ae12646731d759ff06b1e2873924
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-18 01:18:46 -0300

    feat: update products image

--- data/constants/produtos.ts
+++ data/constants/produtos.ts
@@ -1,76 +1,42 @@
 import Produto from "../model/produto";
 
 const produtos: Produto[] = [
-    {
-        nome: "Camiseta",
-        preco: 19.99,
-        descricao: "Camiseta branca",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Camiseta",
-        id: 1,
-    },
-    {
-        nome: "Calça Jeans",
-        preco: 49.99,
-        descricao: "Calça jeans azul escuro",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Cal%C3%A7a+Jeans",
-        id: 2,
-    },
-    {
-        nome: "Tênis Esportivo",
-        preco: 79.99,
-        descricao: "Tênis esportivo confortável para corrida",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=T%C3%AAnis+Esportivo",
-        id: 3,
-    },
-    {
-        nome: "Jaqueta de Couro",
-        preco: 99.99,
-        descricao: "Jaqueta de couro sintético preta",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Jaqueta+de+Couro",
-        id: 4,
-    },
-    {
-        nome: "Óculos de Sol",
-        preco: 29.99,
-        descricao: "Óculos de sol com proteção UV",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=%C3%93culos+de+Sol",
-        id: 5,
-    },
-    {
-        nome: "Relógio de Pulso",
-        preco: 59.99,
-        descricao: "Relógio de pulso analógico",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Rel%C3%B3gio+de+Pulso",
-        id: 6,
-    },
-    {
-        nome: "Bolsa Feminina",
-        preco: 39.99,
-        descricao: "Bolsa feminina de couro sintético",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Bolsa+Feminina",
-        id: 7,
-    },
-    {
-        nome: "Cinto de Couro",
-        preco: 19.99,
-        descricao: "Cinto de couro masculino",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Cinto+de+Couro",
-        id: 8,
-    },
-    {
-        nome: "Chapéu de Palha",
-        preco: 15.99,
-        descricao: "Chapéu de palha estilo praiano",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Chapéu+de+Palha",
-        id: 9,
-    },
-    {
-        nome: "Cachecol de Lã",
-        preco: 25.99,
-        descricao: "Cachecol de lã para o inverno",
-        imagem: "https://via.placeholder.com/150/0000FF/808080?text=Cachecol+de+Lã",
-        id: 10,
-    }
+   {
+      nome: "Camiseta",
+      preco: 19.99,
+      imagem: "https://imgcentauro-a.akamaihd.net/1366x1366/M0V6BY11.jpg",
+      id: 1,
+   },
+   {
+      nome: "Calça",
+      preco: 49.99,
+      imagem: "https://imgcentauro-a.akamaihd.net/768x768/98482602.jpg",
+      id: 2,
+   },
+   {
+      nome: "Tênis Esportivo",
+      preco: 79.99,
+      imagem: "https://imgcentauro-a.akamaihd.net/768x768/M0JT4206.jpg",
+      id: 3,
+   },
+   {
+      nome: "Jaqueta",
+      preco: 99.99,
+      imagem: "https://imgcentauro-a.akamaihd.net/768x768/94908502.jpg",
+      id: 4,
+   },
+   {
+      nome: "Óculos de Sol",
+      preco: 29.99,
+      imagem: "https://imgcentauro-a.akamaihd.net/768x768/985149PN.jpg",
+      id: 5,
+   },
+   {
+      nome: "Relógio",
+      preco: 59.99,
+      imagem: "https://imgcentauro-a.akamaihd.net/768x768/M12NX102.jpg",
+      id: 6,
+   },
 ];
 
 export default produtos;
