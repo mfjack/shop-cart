e434fbcb3b58a12ce13739ae18d98209c2c994e8
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-19 01:03:38 -0300

    style: style header cart

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import { IconBrandAmazon, IconShoppingCart } from "@tabler/icons-react";
 import Link from "next/link";
+import { useContext } from "react";
+import CartContext from "@/data/contexts/cartContext";
 
 const Header = () => {
+   const { amount } = useContext(CartContext);
+
    return (
       <header className="flex justify-between p-5 bg-zinc-500 h-20">
          <Link href="/">
@@ -9,7 +15,7 @@ const Header = () => {
          </Link>
          <Link href="/cart" className="relative">
             <IconShoppingCart size={35} stroke={2} className="text-white" />
-            <div className="absolute w-6 h-6 bg-red-500 rounded-full text-center text-white top-0 right-0 flex items-center text-xs justify-center">10</div>
+            <div className="absolute w-6 h-6 bg-red-500 rounded-full text-center text-white top-0 right-0 flex items-center text-xs justify-center">{amount}</div>
          </Link>
       </header>
    );
